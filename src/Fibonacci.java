82cb9f3c1f4a691bc8517b1fe71e0814cf5b6834
KDepayso <krdepayso@gmail.com>
2022-10-27 13:22:22 +0100

    Added bad example of recursion using fibonacci sequence. Finished challenge

---
+++ src/Fibonacci.java
@@ -0,0 +1,35 @@
+import java.text.MessageFormat;
+
+public class Fibonacci {
+
+    //fibonacci sequence taking an integer as argument
+    //take input as 20
+    //enter user input into recursion fibonacci sequence
+    //save number of times each value has calculated
+    //print count of number of times each value has been calculated
+
+    public static int[] fibonacciCount = new int[21];
+
+
+    public static void main(String[] args){
+        int fibonacci = RecursionFibonacci(20);
+        System.out.println("fibonacci(20) = " + fibonacci);
+        for(int i = 1; i <= 20; i++){
+            System.out.println(MessageFormat.format("{0} calls to fibonacci {1}",fibonacciCount[i], i));
+        }
+
+    }
+
+    public static int RecursionFibonacci(int n){
+        if(n == 0){
+            fibonacciCount[n] = fibonacciCount[n] + 1;
+            return 0;
+        }
+        if(n == 1){
+            fibonacciCount[n] = fibonacciCount[n] + 1;
+            return 1;
+        }
+        fibonacciCount[n] = fibonacciCount[n] + 1;
+        return RecursionFibonacci(n -1 ) + RecursionFibonacci(n - 2);
+    }
+}
